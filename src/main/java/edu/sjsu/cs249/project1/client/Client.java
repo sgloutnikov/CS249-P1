d95af23ba31ab7d803406e889ebea55c9168f63c
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-02-07 00:06:38 -0800

    Add Server and Client stubs. Add gradle build for client and server executable jars

---
+++ src/main/java/edu/sjsu/cs249/project1/client/Client.java
@@ -0,0 +1,10 @@
+package edu.sjsu.cs249.project1.client;
+
+public class Client {
+
+    public static void main(String[] args) {
+        //TODO Prompt for server location. After prompt for navigation options...
+
+        System.out.println("Hello Client...");
+    }
+}
---
+++ src/main/java/edu/sjsu/cs249/project1/server/Server.java
@@ -0,0 +1,10 @@
+package edu.sjsu.cs249.project1.server;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO: On start prompt for bind address and port
+
+        System.out.println("Hello Server...");
+    }
+}
