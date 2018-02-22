625197d2b17f0e4dcfab2b98c4550ebb5298d759
David Fisher <davidfis@cisco.com>
2018-02-21 17:27:29 -0800

    • Changing File to actually write to disk instead of only writing to virtual memory.

--- src/main/java/edu/sjsu/cs249/project1/server/File.java
+++ src/main/java/edu/sjsu/cs249/project1/server/File.java
@@ -1,5 +1,9 @@
 package edu.sjsu.cs249.project1.server;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -10,18 +14,33 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author David Fisher
  */
 public class File {
-    private byte[] data;
+    /**
+     * Note: FILE_PATH_PREFIX is relative to your workspace installation directory.
+     */
+    private static final String FILE_PATH_PREFIX = "filesystem/";
+    private final String absolutePath;
+    private byte[] cachedFile;
     private boolean isActive;
     private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
 
     /**
      * Instantiates a new File with the given data.
      *
+     * @param name
+     *            The name of this file.
      * @param data
      *            The initial data to store in this file.
+     * @throws FileException
+     *             If any problem is encountered while creating the file on disk.
      */
-    public File(final byte[] data) {
-        this.data = data;
+    public File(final String name, final byte[] data) throws FileException {
+        this.absolutePath = FILE_PATH_PREFIX + name;
+        try (FileOutputStream outputStream = new FileOutputStream(this.absolutePath)) {
+            outputStream.write(data);
+        } catch (IOException | SecurityException e) {
+            throw new FileException("Error occurred while creating file.", e);
+        }
+        this.cachedFile = Arrays.copyOf(data, data.length);
         this.isActive = true;
     }
 
@@ -37,7 +56,10 @@ public class File {
         this.lock.readLock().lock();
         try {
             if (this.isActive) {
-                return this.data;
+                /**
+                 * Note: no need to read from the physical file since we have it locally cached in memory.
+                 */
+                return this.cachedFile;
             } else {
                 throw new FileException("This file no longer exists.");
             }
@@ -59,10 +81,19 @@ public class File {
      */
     public boolean modify(final byte[] newData) throws FileException {
         this.lock.writeLock().lock();
-        try {
+        try (FileOutputStream outputStream = new FileOutputStream(this.absolutePath)) {
             if (this.isActive) {
-                if (((this.data != null) && !Arrays.equals(this.data, newData)) || ((this.data == null) && (newData != null))) {
-                    this.data = newData;
+                if (!Arrays.equals(this.cachedFile, newData)) {
+                    /**
+                     * Write to disk.
+                     */
+                    outputStream.write(newData);
+
+                    /**
+                     * Update the cached copy. <br/>
+                     * Note: Must be done after writing to disk in case writing throws an exception.
+                     */
+                    this.cachedFile = Arrays.copyOf(newData, newData.length);
                     return true;
                 } else {
                     return false;
@@ -70,6 +101,8 @@ public class File {
             } else {
                 throw new FileException("This file no longer exists.");
             }
+        } catch (IOException | SecurityException e) {
+            throw new FileException(e);
         } finally {
             this.lock.writeLock().unlock();
         }
@@ -78,7 +111,7 @@ public class File {
     /**
      * Deletes this file. <br/>
      * Lock used: <i>Write</i> <br/>
-     * Note: This object will still exist, but it will be marked as inactive and the data will be nullified.
+     * Note: This object will still exist, but it will be marked as inactive and the file will be deleted.
      *
      * @throws FileException
      *             If this file was already deleted by another process.
@@ -87,11 +120,14 @@ public class File {
         this.lock.writeLock().lock();
         try {
             if (this.isActive) {
-                this.data = null;
+                Files.delete(Paths.get(this.absolutePath));
+                this.cachedFile = null;
                 this.isActive = false;
             } else {
                 throw new FileException("This file no longer exists.");
             }
+        } catch (IOException | SecurityException e) {
+            throw new FileException(e);
         } finally {
             this.lock.writeLock().unlock();
         }
