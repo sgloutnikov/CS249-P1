7ba44ee690ecc57825287c4e09dc94a15b87fec2
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-03-05 09:27:36 -0800

    Load existing files at startup

--- src/main/java/edu/sjsu/cs249/project1/server/FileServer.java
+++ src/main/java/edu/sjsu/cs249/project1/server/FileServer.java
@@ -3,6 +3,9 @@ package edu.sjsu.cs249.project1.server;
 import edu.sjsu.cs249.project1.remote.ClientCallback;
 import edu.sjsu.cs249.project1.remote.FileServerService;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -13,6 +16,26 @@ public class FileServer extends UnicastRemoteObject implements FileServerService
 
     public FileServer() throws RemoteException {
         super();
+        // Load existing files on the file system.
+        System.out.println("Loading existing files on the file system.");
+        String absolutePath = "filesystem/";
+        try {
+            Files.walkFileTree(Paths.get(absolutePath), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
+                    String fileName = file.toFile().getName();
+                    System.out.println("Reading " + fileName);
+                    try {
+                        FileSystem.getInstance().createFile(fileName, Files.readAllBytes(file));
+                    } catch (FileException e) {
+                        e.printStackTrace();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
